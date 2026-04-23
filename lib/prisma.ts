899471cdfd5645a1533c07cd5641ba4b5f2270d8
Elvinoacer <omondielvin@gmail.com>
2026-04-24 00:14:09 +0300

    refactor: update import paths for PrismaClient in prisma.ts and create new seed-voters.js script

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,7 +6,7 @@
  */
 
 import 'dotenv/config';
-import { PrismaClient } from '@/app/generated/prisma/client';
+import { PrismaClient } from '../app/generated/prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
